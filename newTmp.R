5c4aed1f1223898d941f942cd6d78ba2865c135c
Mengdie Wang <mwang3@macalester.edu>
2015-06-09 19:08:00 +0000

    test make_layer function

---
+++ newTmp.R
@@ -0,0 +1,139 @@
+library("shiny")
+
+runApp(
+  list(
+    server = shinyServer(
+      function(input, output, session) {
+        
+        
+        #         geom_list <- reactive({
+        #           if (input$geoms == NULL) {
+        #             print("Please choose a geom")
+        #           }
+        #           else {
+        # selectizeInput("x","Var X", choices = list(1,2,3)),
+        # selectizeInput("y","Var Y", choices = list(1,2,3))
+        #             selectizeInput("col","Choose Colors")
+        #           }          
+        #         })
+        
+#         output$ui <- renderUI({
+#           while (input$show_layer_1 || input$show_layer_2) { 
+#             make_layer()
+#             
+#           }
+#         })
+        
+        
+        
+      }),
+    
+    ui = shinyUI(
+      fluidPage(
+        layer_tab
+      )
+    )
+    
+  )
+)
+
+
+
+# make_layer <- function(){
+#   layer <- tabPanel("layer n",
+#                     wellPanel(selectizeInput("geoms", "Geoms", 
+#                                              choices = list("geom_text" = 1, "geom_line" = 2, "geom_point" = 3)
+#                     ),
+#                     actionButton("plot","Plot"),
+#                     actionButton("reset","Reset")
+#                     #                             geom_list(),
+# #                     conditionalPanel(
+# #                       condition = "input.geoms == 1",
+# #                       selectizeInput("x","Var X", choices = list(1,2,3)),
+# #                       selectizeInput("y","Var Y", choices = list(1,2,3))
+# #                     ) 
+#                     )    
+#                     
+#                     
+#   )       
+#   
+#   return (layer)
+# }  
+
+
+make_layer <- function(){
+  wellPanel(
+    selectizeInput("geoms", "Geoms", 
+                   choices = list("geom_text" = 1, "geom_line" = 2, "geom_point" = 3)
+    ),
+    actionButton("plot","Plot"),
+    actionButton("reset","Reset"),
+    selectizeInput("x","Var X", choices = list(1,2,3)),
+    selectizeInput("y","Var Y", choices = list(1,2,3))
+  )  
+}
+
+
+layer_1_cond <- tabPanel(
+  conditionalPanel(
+    condition = "input.show_layer_1 == true",
+    "Layer 1",
+    browser(),
+    make_layer()
+    )
+)
+
+layer_2_cond <- tabPanel(
+  conditionalPanel(
+    condition = "input.show_layer_2 == true",
+    "Layer 2",
+    make_layer()
+    )
+)
+
+layer_3_cond <- tabPanel(
+  conditionalPanel(
+    condition = "input.show_layer_3 == true",
+    "Layer 3",
+    make_layer()
+    )
+)
+
+layer_4_cond <- tabPanel(
+  conditionalPanel(
+    condition = "input.show_layer_4 == true",
+    "Layer 4",
+    make_layer()
+    )
+)
+
+layer_5_cond <- tabPanel(
+  conditionalPanel(
+    condition = "input.show_layer_5 == true",
+    "Layer 5",
+    make_layer()
+  )
+)
+
+
+layer_tab <- tabPanel(
+  "Layer",
+  fluidRow(
+    tabsetPanel(
+      id="tabset",
+                tabPanel("Manage layers", 
+                         checkboxInput("show_layer_1", "Display Layer 1", value=FALSE),
+                         checkboxInput("show_layer_2", "Display Layer 2", value=FALSE),
+                         checkboxInput("show_layer_3", "Display Layer 3", value=FALSE),
+                         checkboxInput("show_layer_4", "Display Layer 4", value=FALSE),
+                         checkboxInput("show_layer_5", "Display Layer 5", value=FALSE)),
+                #                        create_cond_layer_tab()
+                layer_1_cond,
+                layer_2_cond,
+                layer_3_cond,
+                layer_4_cond,
+                layer_5_cond 
+                
+                
+    )
+  ))
