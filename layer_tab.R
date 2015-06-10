51096518094aa4fcf6766fa7b3a14fec1cba4ac3
Mengdie Wang <mwang3@macalester.edu>
2015-06-10 16:32:01 +0000

    added make_layer and update_Aesthetics function

--- layer_tab.R
+++ layer_tab.R
@@ -1,53 +1,79 @@
-
-make_layer <- function(){
+make_layer <- function(n){
+# The function takes an input n and makes a well panel where allows users to choose from different 
+# geoms types and aesthetics. If the function is called in multiple layers, it will assign unique id to
+# each geom and aesthetics input for future reference
+  
+  id <- paste0("geoms",n)
+  label <- paste0("Geoms",n)
+  controlId <- paste0("controls",n)
+  
   wellPanel(
-    selectizeInput("geoms", "Geoms", 
+    selectizeInput(id, label, 
                    choices = list("geom_line" = 1, "geom_point" = 2, "geom_bar" = 3)
     ),
-    selectizeInput("controls","Select Aesthetics",choices = list()),
+    selectizeInput(controlId,"Select Aesthetics",choices = list()),
     actionButton("plot","Plot"),
     actionButton("reset","Reset")
   )  
 }
 
-layer_1_cond <- tabPanel(
-  conditionalPanel(
-    condition = "input.show_layer_1 == true",
-    "Layer 1"
-  ), 
-  column(4,
-            make_layer()),
-  column(8)
-)
 
-layer_2_cond <- tabPanel(
-  conditionalPanel(
-    condition = "input.show_layer_2 == true",
-    "Layer 2"
-  ),
-  make_layer()
-)
 
-layer_3_cond <- tabPanel(
-  conditionalPanel(
-    condition = "input.show_layer_3 == true",
-    "Layer 3"),
-    make_layer()
-)
+make_tabPanel <- function(n){
+  cond <- paste0("input.show_layer_",n," == true")
+  label <- paste("Layer", n)
+  
+  tabPanel(    
+    conditionalPanel(
+      conditon = cond,
+      label
+    ),    
+    make_layer(n)
+    )
+  
+}
 
-layer_4_cond <- tabPanel(
-  conditionalPanel(
-    condition = "input.show_layer_4 == true",
-    "Layer 4"),
-    make_layer()
-  )
 
-layer_5_cond <- tabPanel(
-  conditionalPanel(
-    condition = "input.show_layer_5 == true",
-    "Layer 5"),
-    make_layer()
-)
+
+# layer_1_cond <- tabPanel(
+#   conditionalPanel(
+#     condition = "input.show_layer_1 == true",
+#     "Layer 1"
+#   ), 
+#   column(4,
+#             make_layer(1)
+#          ),
+#   column(8)
+# )
+# 
+# layer_2_cond <- tabPanel(
+#   conditionalPanel(
+#     condition = "input.show_layer_2 == true",
+#     "Layer 2"
+#   ),
+#   make_layer(2)
+# )
+# 
+# layer_3_cond <- tabPanel(
+#   conditionalPanel(
+#     condition = "input.show_layer_3 == true",
+#     "Layer 3"),
+#     make_layer(3)
+# )
+# 
+# layer_4_cond <- tabPanel(
+#   conditionalPanel(
+#     condition = "input.show_layer_4 == true",
+#     "Layer 4"),
+#     make_layer(4)
+#   )
+# 
+# layer_5_cond <- tabPanel(
+#   conditionalPanel(
+#     condition = "input.show_layer_5 == true",
+#     "Layer 5"),
+#     make_layer(5)
+# )
 
 layer_tab <- 
   tabPanel(
@@ -61,12 +87,15 @@ layer_tab <-
                  checkboxInput("show_layer_3", "Display Layer 3", value=FALSE),
                  checkboxInput("show_layer_4", "Display Layer 4", value=FALSE),
                  checkboxInput("show_layer_5", "Display Layer 5", value=FALSE)),
-        layer_1_cond,
-        layer_2_cond,
-        layer_3_cond,
-        layer_4_cond,
-        layer_5_cond 
-        
+#         layer_1_cond,
+#         layer_2_cond,
+#         layer_3_cond,
+#         layer_4_cond,
+#         layer_5_cond 
+make_tabPanel(1),
+make_tabPanel(2),
+make_tabPanel(3)
+
         
       )
     ))
--- newTmp.R
+++ newTmp.R
@@ -5,22 +5,36 @@ runApp(
   list(
     server = shinyServer(
       function(input, output, session) {
-        observe({
+
+        geom_line_options <- list("color","width")
+        geom_point_options <- list("color", "size","alpha")
+        geom_bar_options <- list("color","type","stat","position")
+        
+    
+        updateAesthetics <- function(n){        
+          # The function takes an input n. It updates the choices of aesthetics based on the type of geom
+          # chosen by the user. 
+                  
+          id <- paste0("geoms",n)
+          controlId <- paste0("controls",n)
           
-          geom_line_options <- list("color","width")
-          geom_point_options <- list("color", "size","alpha")
-          geom_bar_options <- list("color","type","stat","position")
-          
-          choices <-switch(input$geoms,
-                 "1" = geom_line_options,
-                 "2" = geom_point_options,
-                 "3" = geom_bar_options)
-          
-          updateSelectInput(session, "controls",
-                            label = paste("Select aesthetics"),
-                            choices = choices)
-          
-        })
+          observe({          
+            choices <-switch(input[[id]],
+                             "1" = geom_line_options,
+                             "2" = geom_point_options,
+                             "3" = geom_bar_options)
+            
+            updateSelectInput(session, controlId,
+                              label = paste("Select aesthetics"),
+                              choices = choices)
+          })
+        } 
+        
+        for (i in 1:5){
+          updateAesthetics(i)
+        }
+        
+        
       }),
     
     ui = shinyUI(
